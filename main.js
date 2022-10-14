a8e2dd220b7cde2a79cb8731887aa5f8757d6680
Neil Guzman <guzmann001@gmail.com>
2022-10-13 22:45:06 -0400

    My solution

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id ) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            let indexOfID = this.cart.indexOf(id)
+            if (indexOfID > -1) {
+                this.cart.splice(indexOfID, 1)
+            }
+        }
+    },
+    computed: {
+        numInCart() {
+            return this.cart.length
+        }
+    }
 })
